df117ecd711cdf82e0e8cedf1bf13f71c0a18414
Yuki Yamada <yyamada@aiming-inc.com>
2015-09-11 15:05:45 +0900

    DxLib&Sqratのための設定調整

--- ScriptLoader.cpp
+++ ScriptLoader.cpp
@@ -2,7 +2,6 @@
 #include <vector>
 #include "tstring.h"
 #include <string>
-//#include <boost/algorithm/string.hpp>
 
 
 ScriptLoader::ScriptLoader()
@@ -20,7 +19,6 @@ Sqrat::Function ScriptLoader::getFunction(const SQChar* callback_func)
 	// "Reload.seriaize" のように、テーブル内の関数を指す文字列が渡された場合に
 	// ドット区切りで分割して、wordsに格納していく
 	std::vector<tstring> words = split(callback_func, '.');
-	//boost::split(words, callback_func, boost::is_any_of("."));
 
 	// 最初はルートテーブルから
 	Sqrat::Table tbl = Sqrat::RootTable();
